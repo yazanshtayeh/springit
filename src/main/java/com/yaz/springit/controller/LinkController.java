c2034c4ad53214c2c5fd2952bf5f3540d551510e
yazanshtayeh <75503439+yazanshtayeh@users.noreply.github.com>
2022-07-25 11:42:55 +0300

    finished the first part of  the view

--- src/main/java/com/yaz/springit/controller/LinkController.java
+++ src/main/java/com/yaz/springit/controller/LinkController.java
@@ -3,43 +3,65 @@ package com.yaz.springit.controller;
 
 import com.yaz.springit.domain.Link;
 import com.yaz.springit.repository.LinkRepository;
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
+import javax.validation.Valid;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/links")
+@Controller
 public class LinkController {
+    private static final Logger logger = LoggerFactory.getLogger(LinkController.class);
     private LinkRepository linkRepository;
-
     public LinkController(LinkRepository linkRepository) {
         this.linkRepository = linkRepository;
     }
 
-    @RequestMapping("/")
+    @GetMapping("/")
     // list
-    public List<Link> list() {
-        return linkRepository.findAll();
+    public String list(Model model) {
+        model.addAttribute("links",linkRepository.findAll());
+        return "link/list";
     }
-    // CRUD
-    @PostMapping("/create")
-    public Link create(@ModelAttribute Link link) {
-        return linkRepository.save(link);
+    @GetMapping("/link/{id}")
+    public String read(@PathVariable Long id, Model model) {
+        Optional<Link> link=linkRepository.findById(id);
+        if(link.isPresent()){
+            model.addAttribute("link",link.get());
+            model.addAttribute("success",model.containsAttribute("success"));
+            return "link/view";
+        }
+        else
+        return "redirect:/";
     }
-    @GetMapping("/{id}")
-    public Optional<Link> read(@PathVariable Long id) {
-        return linkRepository.findById(id);
+    @GetMapping("/link/submit")
+    public String newLinkForm(Model model) {
+        model.addAttribute("link",new Link());
+        return "link/submit";
     }
-    @PutMapping("/{id}")
-    public Link update(@PathVariable Long id,@ModelAttribute Link link) {
-//        Link linkToUpdate=linkRepository.getById(id);
-//        linkToUpdate.setComments(link.getComments());
-//        linkToUpdate.setTitle(link.getTitle());
-        return linkRepository.save(link);
+
+    @PostMapping("/link/submit")
+    public String createLink(@Valid Link link, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if(bindingResult.hasErrors()){
+            logger.info("Validation error were found while submitting the link");
+            model.addAttribute("link",link);
+            return "link/submit";
+        }
+        else{
+            //save our link
+            linkRepository.save(link);
+            logger.info("link saved successfully");
+            redirectAttributes.addAttribute("id",link.getId()).addFlashAttribute("success",true);
+            return "redirect:/link/{id}";
+        }
     }
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-         linkRepository.deleteById(id);
-    }
+
 }
--- src/main/java/com/yaz/springit/domain/Link.java
+++ src/main/java/com/yaz/springit/domain/Link.java
@@ -1,28 +1,38 @@
 package com.yaz.springit.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import com.yaz.springit.service.BeanUtil;
+import lombok.*;
+import org.hibernate.validator.constraints.URL;
+import org.ocpsoft.prettytime.PrettyTime;
 
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
 @Entity
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class Link extends Auditable{
     @Id
     @GeneratedValue
     private long id;
-    @NonNull
+    @NonNull @NotEmpty(message = "please enter a title")
     private String title;
-    @NonNull
+    @NonNull @NotEmpty(message = "please enter a URL")
+    @URL(message = "please enter a valid URL")
     private String url;
 
 
@@ -30,12 +40,27 @@ public class Link extends Auditable{
     @OneToMany(mappedBy = "link")
     private List<Comment> comments =new ArrayList<>();
 
-    public Link(@NonNull String title, @NonNull String url) {
-        this.title = title;
-        this.url = url;
-    }
+//    public Link(@NonNull String title, @NonNull String url) {
+//        this.title = title;
+//        this.url = url;
+//    }
 
     public void addComment(Comment comment){
         comments.add(comment);
+    }
+
+    public String getDomainName() throws URISyntaxException {
+        URI uri = new URI(this.url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
     }
 }
